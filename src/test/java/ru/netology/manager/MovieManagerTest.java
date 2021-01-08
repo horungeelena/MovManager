413bc27e3df5d1a930f1c5ea06fee1ec38ce9058
Elena Fedoseeva <horungeelena@gmail.com>
2021-01-08 18:05:25 +0200

    Added test

--- src/test/java/ru/netology/manager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -1,24 +1,40 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Movie;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class MovieManagerTest {
     MovieManager manager = new MovieManager();
-    private Movie first = new Movie(1, "Bloodshot", "thriller",true);
+    private Movie first = new Movie(1, "Bloodshot", "thriller", true);
     private Movie second = new Movie(2, "Onward", "cartoon", true);
     private Movie third = new Movie(3, "Hotel Belgrade", "comedy", true);
-    private Movie fourth = new Movie(4, "The Gentlemen", "crime",true);
+    private Movie fourth = new Movie(4, "The Gentlemen", "crime", true);
     private Movie fifth = new Movie(5, "The Invisible Man", "fantasy", true);
-    private Movie sixth = new Movie(6, "Trolls World Tour", "cartoon",false);
-    private Movie seventh = new Movie(7, "Number One","comedy",false);
-    private Movie eighth = new Movie(8,"eighth","none",false);
-    private Movie ninth = new Movie(9,"ninth","none",false);
-    private Movie tenth = new Movie(10,"tenth","none",false);
+    private Movie sixth = new Movie(6, "Trolls World Tour", "cartoon", false);
+    private Movie seventh = new Movie(7, "Number One", "comedy", false);
+    private Movie eighth = new Movie(8, "eighth", "none", false);
+    private Movie ninth = new Movie(9, "ninth", "none", false);
+    private Movie tenth = new Movie(10, "tenth", "none", false);
 
-    Movie[] expected = { tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+    Movie[] expected = {tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+
+    @BeforeEach
+    public void setUp(){
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+        manager.addMovie(eighth);
+        manager.addMovie(ninth);
+        manager.addMovie(tenth);
+    }
 
     @Test
     void giveOutTenMovies() {
@@ -44,7 +60,7 @@ class MovieManagerTest {
         manager.addMovie(second);
         manager.addMovie(third);
         manager.addMovie(fourth);
-        Movie movieAdd = new Movie( 5, "The Invisible Man", "fantasy", true);
+        Movie movieAdd = new Movie(5, "The Invisible Man", "fantasy", true);
         manager.addMovie(movieAdd);
         Movie[] actual = manager.getAddLastMovie();
         Movie[] expected = {new Movie(5, "The Invisible Man", "fantasy", true), fourth, third, second, first};
